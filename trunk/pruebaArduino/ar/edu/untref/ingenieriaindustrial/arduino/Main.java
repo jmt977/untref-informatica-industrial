ac0d9bba9e0790b010a15695a5d53c5a8bc484ae
colowakeman <colowakeman@a6a00eba-f70c-a1a3-aca2-4bc517427395>
2014-06-11 03:44:11 +0000

    nuevo programa para el arduino, nueva clase para manegar sensores en java

--- trunk/pruebaArduino/ar/edu/untref/ingenieriaindustrial/arduino/Main.java
+++ trunk/pruebaArduino/ar/edu/untref/ingenieriaindustrial/arduino/Main.java
@@ -3,23 +3,16 @@ package ar.edu.untref.ingenieriaindustrial.arduino;
 import gnu.io.CommPortIdentifier;
 
 import java.util.Enumeration;
-import java.util.Scanner;
 
 public class Main {
 
 	@SuppressWarnings("rawtypes")
 	private static Enumeration portList;
-	
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-
-		System.out
-				.print("Introduzca la cantidad de valores de temperaturas a promediar: ");
-		int cantidadDeTemperaturas = sc.nextInt();
 
 		portList = CommPortIdentifier.getPortIdentifiers();
 
@@ -29,8 +22,8 @@ public class Main {
 				if (portId.getName().equals("COM6")) {
 					//AverageTemperatureCalculator reader = new AverageTemperatureCalculator(cantidadDeTemperaturas, portId);
 					//reader.startReading();
-					
-					AverageTemperatureReader averageTemperatureReader = new AverageTemperatureReader(cantidadDeTemperaturas, portId);
+
+					CompostManager averageTemperatureReader = new CompostManager(portId);
 					averageTemperatureReader.startReading();
 				}
 			}
