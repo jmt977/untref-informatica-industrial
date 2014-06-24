c8842f31d2fe7a28be63dab7a0661f6ada431f73
colowakeman <colowakeman@a6a00eba-f70c-a1a3-aca2-4bc517427395>
2014-06-24 18:57:32 +0000

    fix sensores

--- pruebaArduino/ar/edu/untref/ingenieriaindustrial/arduino/Main.java
+++ pruebaArduino/ar/edu/untref/ingenieriaindustrial/arduino/Main.java
@@ -17,7 +17,7 @@ public class Main {
 			CommPortIdentifier portId = (CommPortIdentifier) portList.nextElement();
 			if (portId.getPortType() == CommPortIdentifier.PORT_SERIAL) {
 				if (portId.getName().equals("COM6")) {
-					CompostManager compostManager = new CompostManager(portId, 20000, 5000, 30000, 5000, 40000, 5000, 30.0, 29.0, 15.0, 40.0, 10.0, 15.0);
+					CompostManager compostManager = new CompostManager(portId, 200000, 5000, 300000, 5000, 400000, 5000, 30.0, 29.0, 15.0, 40.0, 10.0, 15.0);
 					compostManager.start();
 				}
 			}
